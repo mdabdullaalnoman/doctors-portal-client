86c8a4d085f27138cb7b27f3b730e8bfd45870d2
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2021-02-19 22:28:54 +0600

    done

--- src/components/Shared/SideBar/SideBar.js
+++ src/components/Shared/SideBar/SideBar.js
@@ -21,13 +21,15 @@ const SideBar = () => {
   return (
     <div className="sidebar-bg" id="myDIV" >
       <NavLink to="/dashboard/appointment" activeClassName="selected"><p><span className="mr-3"><BsGrid3X3Gap /></span>Dashboard</p></NavLink>
-      {isDoctor && <div>
-        <NavLink to="/dashboard/appointment" activeClassName="selected" exact><p><span className="mr-3"><BsCalendar /></span>Appointment</p></NavLink>
+      <NavLink to="/dashboard/appointment" activeClassName="selected" exact><p><span className="mr-3"><BsCalendar /></span>Appointment</p></NavLink>
+      {isDoctor &&
+       <div>
         <NavLink to="/allPatient" activeClassName="selected"><p><span className="mr-3"><BsPeople /></span> All Patients</p></NavLink>
         <NavLink to="/Presentation" activeClassName="selected"><p><span className="mr-3"><BsFileEarmarkText /></span>Presentation</p></NavLink>
         <NavLink to="/addDoctor" activeClassName="selected"><p><span className="mr-3"><BsPersonCheck /></span>Add Doctor</p></NavLink>
         <NavLink to="/setting" activeClassName="selected"><p><span className="mr-3"><BsGear /></span>Settings</p></NavLink>
-      </div>}
+      </div>
+      }
     </div>
   );
 };
